d2fc4fada8c16acf5f26ac2a3a40ad5c4148548e
elloelloelloello <31784592+elloelloelloello@users.noreply.github.com>
2017-09-08 21:38:11 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,56 @@
+import urllib.request as get
+
+d = {} # conditions dictionary
+
+class Medication():
+
+    def __init__(self, name, contraMeds, sideEffects, cautions):
+        self.name = name
+        self.contraMeds = contraMeds
+        self.sideEffects = sideEffects
+        self.cautions = cautions
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return (isinstance(other, A) and (self.x == other.x))
+
+    def printer(self):
+        print(1)
+
+class Condition():
+
+    def __init__(self, name, treatments):
+        self.name = name
+        self.treatments = treatments
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return (isinstance(other, A) and (self.name == other.name))
+
+    def printer(self):
+        print(2)
+
+name = 'headache'
+treatments = [Medication('name', ['a', 'b'], ['c', 'd'], ['e', 'f']), Medication('name', ['a', 'b'], ['c', 'd'], ['e', 'f'])]
+
+x = Condition(name, treatments)
+x.printer()
+'''
+URL = 'http://www.rxlist.com/drugs/alpha_a.htm'
+page = get.urlopen(URL)
+xs = str(page.read())
+page.close()
+
+print(xs.find('A-Methapred (Methylprednisolone Sodium Succinate)'))
+
+URL2 = 'http://www.rxlist.com/drugs/alpha_b.htm'
+page2 = get.urlopen(URL2)
+xs2 = str(page2.read())
+page2.close()
+
+print(xs.find('B12 (Liver-Stomach Concentrate With Intrinsic Factor)'))
+'''
